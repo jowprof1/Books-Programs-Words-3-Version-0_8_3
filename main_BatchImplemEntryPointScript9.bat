466191a25538976e135c5a7e0cf0440470c84d93
jowprof1 <157438139+jowprof1@users.noreply.github.com>
2024-04-28 13:41:30 -0700

    work_log_7 Rough Draft pkg vsn 8_0 part 1
    commit part 1
    Work Log:
    _________
    
    main_BatchImpemEntryPointScript9.bat (15 lines)
    main_BatchImplemEntryPointScript10_timer_value_cycle_loop (1103610 lines)

---
+++ main_BatchImplemEntryPointScript9.bat
@@ -0,0 +1,15 @@
+call main.bat
+BooksProgramsWords3_README3.txt
+cd batch scripts
+call compile_and_run_linereader_jd_script_3.bat
+cd ../
+BooksProgramsWords3_RotatingCubeDrawing1.png
+BooksProgramsWords3_RotatingCubeDrawing2.png
+BooksProgramsWords3_RotatingCubeDrawing3.png
+BooksProgramsWords3_RotatingCubeDrawing4.png
+BooksProgramsWords3_RotatingCubeDrawing5.png
+BooksProgramsWords3_RotatingCubeDrawing6.png
+cd batch scripts
+call main_BatchImplemEntryPointScript8_timer_value_cycle_loop.bat
+echo "..."
+echo "compile_and_run_linereader_rotating_cube_jd_script.bat"
